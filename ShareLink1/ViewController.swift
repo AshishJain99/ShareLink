20c163de2f5d507315fed5fa39553f53f192b65b
Ashish Jain <imjainashish@gmail.com>
2023-02-07 10:38:10 +0530

    done

--- ShareLink1/ViewController.swift
+++ ShareLink1/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var buttonUI: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonUI.layer.cornerRadius = 5
     }
 
+    func presentShareSheet(){
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    
+        guard let url = URL(string: "http://pandora.yilstaging.com/writable/uploads/20210127/1611811599_2ac19cd41e8387119d7e.mp3")
+        else{
+            return
+        }
+        let shareSheetVC = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        shareSheetVC.popoverPresentationController?.sourceView = sender
+        shareSheetVC.popoverPresentationController?.sourceRect = sender.frame
+        present(shareSheetVC, animated: true)
 
+        
+    }
 }
 
